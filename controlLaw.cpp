17490df810963a08a658abb2b1882cbc2e81af54
poparrish <parkerparrish@u.boisestate.edu>
2018-12-17 13:14:15 -0700

    Update controlLaw.cpp

--- controlLaw.cpp
+++ controlLaw.cpp
@@ -1,101 +1,145 @@
-#include <Arduino.h>
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BNO055.h>
+#include <utility/imumaths.h>
 #include "controlLaw.h"
+#include "pinDef.h"
 
+////pins
+//int vrPin = 5;
+//int tachSignal = 3;//must use either pins 2 or 3 for interrupts if on arduino Uno
+//int zfPin = 2;
+////Gyro connections for Adafruit driver
+////   ===========
+////   Connect SCL to analog 5
+////   Connect SDA to analog 4
+////   Connect VDD to 3.3V DC
+////   Connect GROUND to common ground
 
-controlLaw::controlLaw(){
-  Hz = 15;
-  pulsesPerRot = 45;
-  DerRPMerror = 0;
-  lastCycleSpeedCheck = 0;
-  lastCycleRPM = 0;
-  cutoff = 10;
-  speedCheck = 0;
-  speedCheckFloat = 0.0;
-  Pgain = 0.0;
-  Dgain = 0.0;
-  RPMerror = 0;
-  lastHubPerror = 0;
-  lastDesiredRPM = 0;
-  backToZero = false;
-  lastCycleRPM = 0;
+int Hz = 15;
+
+/* Set the delay between fresh samples */
+#define BNO055_SAMPLERATE_DELAY_MS (10)
+Adafruit_BNO055 bno = Adafruit_BNO055();
+
+//inits for hub interrupt routine
+unsigned long beginningTic = millis();
+int countWheel = 0;
+float sumWheelTime = 0;
+unsigned long currentTime = 0;
+unsigned long lastIntTime = millis();
+boolean firstPass = true;
+boolean backToZero = false;
+float avgDeltaTime = 10000001;
+int iWheel;
+float deltaTime[40];
+unsigned long lastIntTimeCp;
+
+/*
+ * This method is called when the interrupt for wheel 1's hub (hall sensor) goes high. It is calculating the average time 
+ * between interrupts to get an RPM. However, it needs to handle the case when the wheel is stopped and starts up. Hence 
+ * the first interrupt after it has been stopped can't be used to calculate a time bewtween interrups (we need two), so  
+ * teh first time we will calculate a time between intrrupts and get an RPM is during the second interrupt.
+ */
+void hubEncoder(){
+  if(firstPass or backToZero){
+    lastIntTime = millis();
+    avgDeltaTime = 10000001;
+    firstPass = false;
+    backToZero = false;
+  }else{
+    currentTime=millis();
+    deltaTime[countWheel] = (float)(currentTime-lastIntTime);
+    lastIntTime=currentTime;
+
+    for(iWheel = 0; iWheel <= countWheel; iWheel++){
+      sumWheelTime = sumWheelTime+deltaTime[iWheel];
+      //Serial.println(deltaTime[iWheel]);
+    }
+    avgDeltaTime=sumWheelTime/(float)(countWheel+1);
+    sumWheelTime=0;
+    countWheel++;
+  }
 }
 
-returnVariables controlLaw::calculate(float phi,int newDesiredRPM,bool lastInterruptTime,float avgDeltaTime) {
-  using namespace std;
+//pointer to wheel object
+controlLaw* wheel; 
 
-  /*
- * The next section looks to see if the desired RPM is in the same direction or not. IF it is not, it will set the 
- *  desired RPM to 0 this pass and then next pass if will switch the forward and back variable and the desired RPM.
- *  By setting the desried RPM to 0, we also we restart the interrupt logic and go open loop on the initial speedcheck
- *  value (set later on). 
- */
+//arduino setup, runs ones
+void setup() {
+  //setup the pins&set baudrate
+  Serial.begin(115200);
+  pinMode(vrPin, OUTPUT);
+  pinMode(zfPin, OUTPUT);
+  pinMode(tachSignal,INPUT);
+  attachInterrupt(digitalPinToInterrupt(tachSignal),hubEncoder,RISING);
+  interrupts();  
 
-  if (newDesiredRPM < 0)
+  /* Initialise the sensor */
+  if(!bno.begin())
   {
-    if (lastDesiredRPM > 0) {
-      // current positive new negative
-      desiredRPM = 0;
-    }
-    else {
-      // current negative new negative
-      forwardBackward = LOW;
-      desiredRPM =newDesiredRPM*-1;
-    }
+    /* There was a problem detecting the BNO055 ... check your connections */
+    Serial.print("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
+    while(1);
   }
-  else {
-    if (lastDesiredRPM < 0) {
-      // current negative new positive
-      desiredRPM = 0; 
-    }
-    else {
-      //current positive new positive
-      desiredRPM = newDesiredRPM;
-      forwardBackward = HIGH;
-    }
-  }
+  bno.setExtCrystalUse(true);
+  
+  //instantiate new wheel object
+  wheel = new controlLaw();  
+}
 
-  lastDesiredRPM = newDesiredRPM;
+//main loop inits
+float AverageDeltaTime = 10000001;
+unsigned long lastInterruptTime;
 
-/* 
-*  This if statement is looking for the case that the wheel has stopped and we are not getting any more interrupts, 
-*  and averagedeltatime is not valid (old). So, in this case we will set the current RPM to zero. If not, then we will
-*  calcultate the RPM - the 60,000 is to convert from millisec to minutes.
-*/
-  if (( millis() - lastInterruptTime) > 100) {
-    curRPM = 0;
-  }
-  else {
-    curRPM = (1 / ((float)avgDeltaTime * pulsesPerRot)) * 60000;
-  }
-  // If the wheel has stopped, we need to do a new start up sequence.
-  if (curRPM <= cutoff) {
-    backToZero = true;
-  }else{
-    backToZero = false;
-  }
-
-  // Calculate the delta (error) in RPM and in the derivative of RPM.
-  RPMerror = desiredRPM - curRPM;
-  DerRPMerror = curRPM - lastCycleRPM;
-
-//  speedCheck = phi;
-//    HUB PD START//
-  if(desiredRPM != 0){
-    speedCheck = desiredRPM+cutoff;
-  }else{
-    speedCheck = 0;
-  }
-  //keep within 255char size
-  if(speedCheck > 255){
-    speedCheck =255;
-  }
- 
+//arduino main loop
+void loop() {
+  
+  if (millis() - beginningTic > 1000 / Hz){
+    beginningTic = millis();//reset the last time loop executed
+    
    
-// return the desired parameters  
-  struct returnVariables returnStruct;
-  returnStruct.speedCheck = speedCheck;
-  returnStruct.forwardBackward = forwardBackward;
-  returnStruct.currentWheelRPM = curRPM;;
-  returnStruct.backToZero = backToZero;
-  return returnStruct;   
+  
+    //shut ISR off to copy data avoiding read/write corruption
+    noInterrupts();
+    float avgDeltaTimeCp = avgDeltaTime;
+    lastIntTimeCp = lastIntTime;
+    countWheel=0;
+    interrupts();
+
+    //get euler angle
+    imu::Vector<3> euler = bno.getVector(Adafruit_BNO055::VECTOR_EULER);
+    float phi = euler.z();
+    Serial.println(phi);
+
+    //calculate new desiredRPM
+    float newDesiredRPM;
+    newDesiredRPM = phi*2;
+
+    //call the method for wheel calculations
+    struct returnVariables returnVariablesWheel = wheel->calculate(phi,newDesiredRPM,lastInterruptTime,avgDeltaTime);
+    
+    
+    analogWrite(vrPin,returnVariablesWheel.speedCheck);
+    digitalWrite(zfPin,returnVariablesWheel.forwardBackward);
+    Serial.print("speedCheck: ");
+    Serial.println(returnVariablesWheel.speedCheck);
+    
+      //write speed to wheel    
+  //    Serial.print("distanceTraveled: ");
+  //    Serial.println(distanceTraveled);
+  //    Serial.print("curRPM: ");
+  //    Serial.println(curRPM);
+  //    Serial.print("desiredRPM: ");
+  //    Serial.println(desiredRPM);
+  //    analogWrite(vrPin,speedCheck);
+  //    digitalWrite(zfPin,HIGH);
+
+  
+  }
+
+
+  
+  
+  
 }
